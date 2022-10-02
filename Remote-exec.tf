4a75e9bbc66d5de09cb78a3f26d51e68e5889b75
Gopinath <100778593+GopinathVedha@users.noreply.github.com>
2022-10-02 23:52:43 +0530

    Update Remote-exec.tf

--- Remote-exec.tf
+++ Remote-exec.tf
@@ -1,15 +1,13 @@
- connection {
+provisioner "remote-exec" {
+        connection {
         type = "ssh"
         host = self.public_ip
         user = "ec2-user"
-        private_key = file("/home/XXX/Downloads/XXX.pem")
-
-       
-    }
-    provisioner "remote-exec" {
+        private_key = file("/home/gopinath/Downloads/Demo.pem")
+        }
        inline = [
-        "hostnamectl set-hostname 'gfs-server-03'"
+        "sudo hostnamectl set-hostname 'gopinath-server'"
        ]
         
     }
- depends_on = [aws_instance.XXX]
+ depends_on = [aws_instance.example]
